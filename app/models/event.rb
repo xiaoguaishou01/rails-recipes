481def90efba74b9795c3f4a3a3daa618180156e
韦月炎 <weiyueyan@MBP.local>
2017-08-14 17:06:17 +0800

    6-2 显示输出

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,6 +4,8 @@ class Event < ApplicationRecord
  before_validation :generate_friendly_id, :on => :create
  validates_uniqueness_of :friendly_id
  validates_format_of :friendly_id, :with => /\A[a-z0-9\-]+\z/
+ STATUS = ["draft", "public", "private"]
+ validates_inclusion_of :status, :in => STATUS
 
  def to_param
    self.friendly_id
