992ac4f241bc7ad2f32acfff6e54f51300022f00
韦月炎 <weiyueyan@MBP.local>
2017-08-16 17:05:22 +0800

    10-2 嵌套（nested）表单 UI

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,7 +7,8 @@ class Event < ApplicationRecord
  STATUS = ["draft", "public", "private"]
  validates_inclusion_of :status, :in => STATUS
  belongs_to :category, :optional => true
- has_many :tickets, :dependent => :destroy
+ has_many :tickets, :dependent => :destroy, :inverse_of => :event
+ accepts_nested_attributes_for :tickets, :allow_destroy => true, :reject_if => :all_blank
 
  def to_param
    self.friendly_id
