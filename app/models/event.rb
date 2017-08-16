e9784e90bde934fad96ae5e727dc4a3ea8c5b882
韦月炎 <weiyueyan@MBP.local>
2017-08-16 16:24:23 +0800

    情境准备

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,6 +7,7 @@ class Event < ApplicationRecord
  STATUS = ["draft", "public", "private"]
  validates_inclusion_of :status, :in => STATUS
  belongs_to :category, :optional => true
+ has_many :tickets, :dependent => :destroy
 
  def to_param
    self.friendly_id
