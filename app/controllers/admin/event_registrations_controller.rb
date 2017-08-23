385615bef030c54d458727fba82c0034ad78c814
韦月炎 <weiyueyan@MBP.local>
2017-08-23 19:36:58 +0800

    20-1汇出CSV档案

--- app/controllers/admin/event_registrations_controller.rb
+++ app/controllers/admin/event_registrations_controller.rb
@@ -1,4 +1,5 @@
-class Admin::EventRegistrationsController < ApplicationController
+require 'csv'
+class Admin::EventRegistrationsController < AdminController
   before_action :find_event
 
   def index
@@ -22,6 +23,20 @@ class Admin::EventRegistrationsController < ApplicationController
    end
    if params[:registration_id].present?
      @registrations = @registrations.where( :id => params[:registration_id].split(","))
+   end
+
+   respond_to do |format|
+     format.html
+     format.csv {
+       @registrations = @registrations.reorder("id ASC")
+       csv_string = CSV.generate do |csv|
+         csv << ["报名ID", "票种","姓名","状态","Email", "报名时间"]
+         @registrations.each do |r|
+           csv << [r.id, r.ticket.name, r.name, t(r.status, :scope => "registration.status"), r.email, r.created_at]
+         end
+       end
+       send_data csv_string, :filename => "#{@event.friendly_id}-registrations-#{Time.now.to_s(:number)}.csv"
+     }
    end
   end
 
